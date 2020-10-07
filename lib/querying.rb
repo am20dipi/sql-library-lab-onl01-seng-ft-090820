c193d7f7c27d289dd3ec3ce04700945a7ec7d743
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-10-07 23:36:19 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,7 +1,7 @@
 def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT books.title, books.year
   FROM books
-  INNER JOIN series
+  JOIN series
   ON books.id = books.series_id
   WHERE books.id = 1
   ORDER BY books.year ASC;"
